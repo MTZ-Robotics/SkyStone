c904ae77a0037f9a8698701e6c1ba3f40687fc78
Karl W. Scherer <47581330+karlscherer@users.noreply.github.com>
2019-10-28 15:01:59 -0500

    Update CoachSchererVuforiaSkyStoneNavigation.java
    Modified the name of the mode for the phone

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CoachSchererVuforiaSkyStoneNavigation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CoachSchererVuforiaSkyStoneNavigation.java
@@ -82,7 +82,7 @@ import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocaliz
  */
 
 
-@TeleOp(name="SKYSTONE Vuforia Nav", group ="Concept")
+@TeleOp(name="KWS - SKYSTONE Vuforia Nav", group ="Concept")
 //@Disabled
 public class CoachSchererVuforiaSkyStoneNavigation extends LinearOpMode {
 
