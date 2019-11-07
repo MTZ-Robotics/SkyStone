2b912083c158e94a64ba7a90a379136f6e30fbbe
karlscherer <kwscherer@gmail.com>
2019-11-06 19:34:31 -0600

    Create DriverControlOpMTZ.java
    Here is our (untested) driver control op for the first competition!  We still need to add a constant to the arm power so that it doesn't collapse on itself

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlOpMTZ.java
@@ -0,0 +1,100 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+
+public class DriverControlOpMTZ extends LinearOpMode {
+
+    private DcMotor frontRight;
+    private DcMotor backRight;
+    private DcMotor frontLeft;
+    private DcMotor backLeft;
+    private DcMotor arm;
+
+    Servo servo;
+    double servoPosition = 0.0;
+
+    @Override
+
+    public void runOpMode(){
+
+        frontLeft=hardwareMap.dcMotor.get("frontLeft");
+        frontRight=hardwareMap.dcMotor.get("frontRight");
+        backLeft=hardwareMap.dcMotor.get("backLeft");
+        backRight=hardwareMap.dcMotor.get("backRight");
+        arm=hardwareMap.dcMotor.get("arm");
+        servo=hardwareMap.servo.get("claw");
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+        servo.setPosition(servoPosition);
+
+        double drivePower=1;
+        double driveDirection=1;
+        boolean aButtonPressed=false;
+        boolean bButtonPressed=false;
+
+        stat("HardwareMap Complete");
+
+        waitForStart();
+
+        while(opModeIsActive()){
+
+            if(gamepad1.a){
+                aButtonPressed=true;
+            }
+            else if(aButtonPressed){
+                aButtonPressed=false;
+                if(drivePower==0.5){
+                    drivePower=1;
+                }
+                else{
+                    drivePower=0.5;
+                }
+            }
+
+            if(gamepad1.b){
+                bButtonPressed=true;
+            }
+            else if(bButtonPressed){
+                bButtonPressed=false;
+                if(driveDirection==-1){
+                    driveDirection=1;
+                }
+                else{
+                    driveDirection=-1;
+                }
+            }
+
+            stat(new String[]{"Drive Power (A): "+drivePower,"Drive Direction (B): "+driveDirection});
+
+            backLeft.setPower(drivePower*(driveDirection*(gamepad1.right_stick_y+gamepad1.left_stick_x)-gamepad1.right_stick_x));
+            backRight.setPower(drivePower*(driveDirection*(gamepad1.right_stick_y-gamepad1.left_stick_x)+gamepad1.right_stick_x));
+            frontLeft.setPower(drivePower*(driveDirection*(-gamepad1.right_stick_y+gamepad1.left_stick_x)+gamepad1.right_stick_x));
+            frontRight.setPower(drivePower*(driveDirection*(-gamepad1.right_stick_y-gamepad1.left_stick_x)-gamepad1.right_stick_x));
+            arm.setPower(gamepad2.left_stick_y);
+            servo.setPosition(gamepad2.right_stick_y);
+        }
+    }
+
+    public void stat(String[] in){
+        for(String a : in){
+            telemetry.addData("Status",a);
+        }
+        telemetry.update();
+    }
+
+    public void stat(String in){
+        telemetry.addData("Status",in);
+        telemetry.update();
+    }
+
+    public void motorstop(){
+        for(DcMotor M : new DcMotor[]{frontLeft,backLeft,frontRight,backRight,arm}){
+            M.setPower(0);
+        }
+    }
+}
