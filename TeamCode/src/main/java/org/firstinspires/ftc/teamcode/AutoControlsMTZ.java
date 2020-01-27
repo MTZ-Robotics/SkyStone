1620a998ca17455fc7dfef8d0472113af5a617d9
karlscherer <kwscherer@gmail.com>
2020-01-26 21:30:22 -0600

    Archived AutoControlsMTZ and refactored AutoControlsMTZ_test to AutoControlsMTZ

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoControlsMTZ.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoControlsMTZ.java
@@ -1,16 +1,42 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@Autonomous(name ="Auto Controls", group = "z_test")
+@Autonomous(name ="Auto Controls Test", group = "z_test")
 
 //@Disabled
 
 public class AutoControlsMTZ extends LinearOpMode {
 
+
+    /**************
+     *
+     * Modify these speeds to help with diagnosing drive errors
+     *
+     **************/
+    private static final double defaultDriveSpeed = 0.4;
+    private static final double defaultTurnSpeed = 0.4;
+    private static final int defaultPauseTime = 200;
+
+
+    /**********************
+     * These variables are the constants in path commands
+     **********************/
+    private static final double ticksPerRevolution = 145.6;
+    private static final double gearReduction = 2.0;
+    private static final double wheelDiameterInches = 4.0;
+    private static final double pi = 3.1415;
+    private static final double conversionTicksToInches = (ticksPerRevolution * gearReduction) / (pi * wheelDiameterInches);
+    private static final double experimentalInchesPerTurn = 91.8;
+    private int allianceReverser = 1;
+
+    /*****************
+     * Declare motor & servo objects
+     ****************/
     private DcMotor frontRight;
     private DcMotor backRight;
     private DcMotor frontLeft;
@@ -21,30 +47,18 @@ public class AutoControlsMTZ extends LinearOpMode {
     private Servo rightHook;
     private Servo blockThrower;
 
-    private static final double ticksPerRevolution = 145.6;
-    private static final double gearReduction = 2.0;
-    private static final double wheelDiameterInches = 4.0;
-    private static final double pi = 3.1415;
-    private static final double conversionTicksToInches = (ticksPerRevolution * gearReduction) / (pi * wheelDiameterInches);
-    private static final double experimentalInchesPerTurn = 91.8;
-    private int allianceReverser = 1;
 
-    /**************
-     *
-     * Modify these speeds to help with diagnosing drive errors
-     *
-     **************/
-    private static final double defaultDriveSpeed = 0.1;
-    private static final double defaultTurnSpeed = 0.1;
-    private static final int defaultPauseTime = 1000;
+    /***********
+     * Lights Control Declarations
+     ***********/
 
-
+    RevBlinkinLedDriver blinkinLedDriver;
+    RevBlinkinLedDriver.BlinkinPattern pattern;
 
     @Override
 
     public void runOpMode() throws InterruptedException {
-        Boolean oldArm = true;
-        autoPaths("Blue","FoundationBridge",oldArm);
+        autoPaths("Blue","FoundationWall",false);
 
     }
 
@@ -75,6 +89,24 @@ public class AutoControlsMTZ extends LinearOpMode {
         //arm.setDirection(DcMotor.Direction.REVERSE);
         leftHook.setDirection(Servo.Direction.REVERSE);
 
+
+        /*************
+         * Set Lights Variables to the color for the alliance
+         *************/
+        blinkinLedDriver = hardwareMap.get(RevBlinkinLedDriver.class, "blinkin");
+
+        if (alliance=="Blue") {
+
+            pattern = RevBlinkinLedDriver.BlinkinPattern.HEARTBEAT_BLUE;
+        } else if (alliance=="Red") {
+            pattern = RevBlinkinLedDriver.BlinkinPattern.HEARTBEAT_RED;
+        }
+        blinkinLedDriver.setPattern(pattern);
+
+
+        /********
+         * Movement starts here on initialize
+         */
         leftHook.setPosition(0.5);
         rightHook.setPosition(0.5);
 
@@ -84,59 +116,247 @@ public class AutoControlsMTZ extends LinearOpMode {
          * Raise the arm to fit within 18" long dimension
          ************/
         if (oldArm) {
-           RaiseArm(14,100);
+           RaiseArm(14,defaultPauseTime/4);
         }
         telemetry.log().clear();
         telemetry.update();
         telemetry.log().add(pathToRun+" Initialized. Go "+alliance+" alliance");
 
 
-        if (pathToRun=="FoundationBridge") {
+        //Paths written for Blue alliance and reverse turns if on Red alliance
+        allianceReverser=1;
+        if (alliance=="Red") {
+            allianceReverser=-1;
+        }
 
+
+
+        /************************************************************
+         * Paths            Paths            Paths          Paths   *
+         ************************************************************/
+        //Write paths for Blue alliance and apply reverser on turns and strafes
+
+        if (pathToRun=="FoundationBridge" || pathToRun=="FoundationWall" ) {
+
+            /************************************
+             * Path set up -- Add to each path
+             ***********************************/
+            //Robot Setup Notes
             telemetry.log().add("Robot should face towards wall centered in tile next to bridge.");
 
             waitForStart();
 
-            allianceReverser=1;
-            if (alliance=="Red") {
-                allianceReverser=-1;
+            //Turn lights off
+            if (alliance=="Blue") {
+                pattern = RevBlinkinLedDriver.BlinkinPattern.BLACK;
+            } else if (alliance=="Red") {
+                pattern = RevBlinkinLedDriver.BlinkinPattern.BLACK;
             }
+            blinkinLedDriver.setPattern(pattern);
 
-            //Align Hooks With Foundation
-            Drive(-24, 0.12, 200);
-            Strafe(allianceReverser*12, 0.12, 200);
-            Drive(-5, 0.12, 200);
+            /************
+             * Path Start
+             ************/
 
-            //Hook Foundation
-            HooksDown();
-
-            //Move Foundation to Build Zone
-            Drive(20, 0.2, 200);
-            Turn(allianceReverser*-40, 0.2, 200);
-            Drive(5, 0.2, 200);
-            Turn(allianceReverser*-80, 0.2, 200);
-            Strafe(allianceReverser*-5, 0.2, 200);
-            Drive(-12, 0.1, 200);
-
-            //Unhook Foundation
-            HooksUp();
+            goToFoundationfromWall(allianceReverser);
+            moveFoundation(allianceReverser);
 
             //Align to Park
-            Strafe(allianceReverser*-8, 0.1, 200);
-            Drive(24, 0.1, 200);
+            if (pathToRun=="FoundationBridge") {
+                Strafe(allianceReverser * -8, defaultDriveSpeed, defaultPauseTime);
+            } else {
+                Strafe(allianceReverser * 12,0.1,200);
+            }
+            //Forward to bridge area
+            Drive(24, defaultDriveSpeed, defaultPauseTime);
 
             //Lower arm gracefully
-            LowerArm(10, 1000);
+            LowerArm(10, defaultPauseTime*2);
 
             //Raise arm a little
-            RaiseArm(3, 200);
+            RaiseArm(3, defaultPauseTime);
 
             //Park
-            Drive(14, 0.1, 0);
+            Drive(14, defaultDriveSpeed/2, 0);
+        } else if (pathToRun=="DepotWall" || pathToRun=="DepotBridge") {
+            /************************************
+             * Path set up -- Add to each path
+             ***********************************/
+            //Robot Setup Notes
+            telemetry.log().add("Robot starts facing quarry on intersection between tiles one tile away from bridge line.");
+
+            waitForStart();
+
+            //Turn lights off
+            if (alliance=="Blue") {
+                pattern = RevBlinkinLedDriver.BlinkinPattern.BLACK;
+            } else if (alliance=="Red") {
+                pattern = RevBlinkinLedDriver.BlinkinPattern.BLACK;
+            }
+            blinkinLedDriver.setPattern(pattern);
+
+            /************
+             * Path Start
+             ************/
+
+            //Wait 15 seconds
+            sleep(15000);
+
+            if (pathToRun=="DepotWall"){
+                //Move forward slightly
+                Drive(1,defaultDriveSpeed,defaultPauseTime);
+            } else {
+                //Move forward more
+                Drive(12,defaultDriveSpeed,defaultPauseTime);
+            }
+
+            //Lower arm
+            LowerArm(10,defaultPauseTime*2);
+            RaiseArm(3,defaultPauseTime);
+
+            //Park
+            Strafe(allianceReverser * -24,defaultDriveSpeed,0);
+
         }
     }
 
-//Encoder Functions
+    //Path Methods
+    public void goToFoundationfromWall(int allianceReverser) throws InterruptedException{
+
+        //Align Hooks With Foundation
+        Drive(-24, defaultDriveSpeed, defaultPauseTime);
+        Strafe(allianceReverser*12, defaultDriveSpeed, defaultPauseTime);
+        Drive(-5, defaultDriveSpeed, defaultPauseTime);
+    }
+
+    public void moveFoundation (int allianceReverser) throws InterruptedException{
+
+        //Hook Foundation
+        HooksDown();
+
+        //Move Foundation to Build Zone
+        Drive(20, 0.2, defaultPauseTime);
+        Turn(allianceReverser * -40, 0.2, defaultPauseTime);
+        Drive(5, 0.2, defaultPauseTime);
+        Turn(allianceReverser * -80, 0.2, defaultPauseTime);
+        Strafe(allianceReverser * -5, 0.2, defaultPauseTime);
+        Drive(-12, 0.1, defaultPauseTime);
+
+        //Unhook Foundation
+        HooksUp();
+    }
+
+    //Motion Methods
+
+    public void Drive(int distance, double motorPower, int pause) throws InterruptedException {
+        if (opModeIsActive()) {
+            StopAndResetDriveEncoders();
+            DriveByInches(distance);
+            RunDriveToPosition();
+            DrivePower(motorPower);
+            while (frontLeft.isBusy() && frontRight.isBusy() && backLeft.isBusy() && backRight.isBusy()) {
+                DisplayDriveTelemetry();
+            }
+            DrivePower(0);
+            Thread.sleep(pause);
+        }
+    }
+    public void Strafe(int distance, double power, int pause) throws InterruptedException {
+        if (opModeIsActive()) {
+            StopAndResetDriveEncoders();
+            StrafeByInches(distance);
+            RunDriveToPosition();
+            DrivePower(power);
+            while (frontLeft.isBusy() && frontRight.isBusy() && backLeft.isBusy() && backRight.isBusy()) {
+                DisplayDriveTelemetry();
+            }
+            DrivePower(0);
+            Thread.sleep(pause);
+        }
+    }
+    public void Turn(int degrees, double power, int pause) throws InterruptedException {
+        if (opModeIsActive()) {
+            StopAndResetDriveEncoders();
+            TurnByAngle(degrees);
+            RunDriveToPosition();
+            DrivePower(power);
+            while (frontLeft.isBusy() && frontRight.isBusy() && backLeft.isBusy() && backRight.isBusy()) {
+                DisplayDriveTelemetry();
+            }
+            DrivePower(0);
+            Thread.sleep(pause);
+        }
+    }
+    public void RaiseArm(int distance, int pause) throws InterruptedException {
+        //Use time based arm controls since the arm falls when the target distance is reached
+
+        arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        arm.setPower(0.4);
+        //1000 ms = 12 inches
+        sleep(distance * 1000/12);
+        //sleep(1000);
+        arm.setPower(0.2);
+        Thread.sleep(pause);
+    }
+
+    public void LowerArm(int distance, int pause) throws InterruptedException {
+        arm.setPower(0.1);
+        sleep(distance * 100);
+        arm.setPower(0);
+        Thread.sleep(pause);
+    }
+    public void HooksDown() {
+        //Light Reverse Power On
+        lightReverse();
+        sleep(500);
+
+        leftHook.setPosition(0);
+        rightHook.setPosition(0);
+        sleep(1500);
+
+        //Reverse Power Off
+        StopAndResetDriveEncoders();
+    }
+    public void HooksUp() {
+        leftHook.setPosition(0.5);
+        rightHook.setPosition(0.5);
+        sleep(1500);
+    }
+    public void lightReverse(){
+        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        DrivePower(-0.1);
+    }
+
+    //Telemetry Methods
+
+    public void DisplayDriveTelemetry() {
+        double frontLeftInches = frontLeft.getCurrentPosition() / conversionTicksToInches;
+        double frontRightInches = frontRight.getCurrentPosition() / conversionTicksToInches;
+        double backLeftInches = backLeft.getCurrentPosition() / conversionTicksToInches;
+        double backRightInches = backRight.getCurrentPosition() / conversionTicksToInches;
+        telemetry.clear();
+        telemetry.addLine()
+                .addData("Front Left Inches ", (int) frontLeftInches + "   Power: " + "%.1f", frontLeft.getPower());
+        telemetry.addLine()
+                .addData("Front Right Inches: ", (int) frontRightInches + "   Power: " + "%.1f", frontRight.getPower());
+        telemetry.addLine()
+                .addData("Back Left Inches: ", (int) backLeftInches + "   Power: " + "%.1f", backLeft.getPower());
+        telemetry.addLine()
+                .addData("Back Right Inches: ", (int) backRightInches + "   Power: " + "%.1f", backRight.getPower());
+        telemetry.update();
+    }
+    public void DisplayArmTelemetry() {
+        double armInches = arm.getCurrentPosition() / conversionTicksToInches;
+        telemetry.clear();
+        telemetry.addLine()
+                .addData("Arm Inches ", (int) armInches + "  Power: " + "%.1f", arm.getPower());
+        telemetry.update();
+    }
+
+//Encoder Methods
 
     public void StopAndResetAllEncoders() {
         frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -145,15 +365,12 @@ public class AutoControlsMTZ extends LinearOpMode {
         backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
-
     public void StopAndResetDriveEncoders() {
         frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
-
-
     public void RunDriveToPosition() {
         frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -161,8 +378,7 @@ public class AutoControlsMTZ extends LinearOpMode {
         backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
-
-//Distance Calculation Functions
+//Distance Calculation Methods
 
     public void DriveByInches(int distance) {
         frontLeft.setTargetPosition(distance * (int) conversionTicksToInches);
@@ -185,8 +401,7 @@ public class AutoControlsMTZ extends LinearOpMode {
         backRight.setTargetPosition(degrees * (int) conversionTicksToInches * (int) experimentalInchesPerTurn / 360);
     }
 
-
-//Power Functions
+//Power Methods
 
     public void DrivePower(double power) {
         frontLeft.setPower(power);
@@ -194,80 +409,6 @@ public class AutoControlsMTZ extends LinearOpMode {
         backLeft.setPower(power);
         backRight.setPower(power);
     }
-
-
- //Telemetry Functions
-
-    public void DisplayDriveTelemetry() {
-        double frontLeftInches = frontLeft.getCurrentPosition() / conversionTicksToInches;
-        double frontRightInches = frontRight.getCurrentPosition() / conversionTicksToInches;
-        double backLeftInches = backLeft.getCurrentPosition() / conversionTicksToInches;
-        double backRightInches = backRight.getCurrentPosition() / conversionTicksToInches;
-        telemetry.clear();
-        telemetry.addLine()
-                .addData("Front Left Inches ", (int) frontLeftInches + "   Power: " + "%.1f", frontLeft.getPower());
-        telemetry.addLine()
-                .addData("Front Right Inches: ", (int) frontRightInches + "   Power: " + "%.1f", frontRight.getPower());
-        telemetry.addLine()
-                .addData("Back Left Inches: ", (int) backLeftInches + "   Power: " + "%.1f", backLeft.getPower());
-        telemetry.addLine()
-                .addData("Back Right Inches: ", (int) backRightInches + "   Power: " + "%.1f", backRight.getPower());
-        telemetry.update();
-    }
-
-    public void DisplayArmTelemetry() {
-        double armInches = arm.getCurrentPosition() / conversionTicksToInches;
-        telemetry.clear();
-        telemetry.addLine()
-                .addData("Arm Inches ", (int) armInches + "  Power: " + "%.1f", arm.getPower());
-        telemetry.update();
-    }
-
-//Motion Functions
-
-    public void Drive(int distance, double motorPower, int pause) throws InterruptedException {
-        if (opModeIsActive()) {
-            StopAndResetDriveEncoders();
-            DriveByInches(distance);
-            RunDriveToPosition();
-            DrivePower(motorPower);
-            while (frontLeft.isBusy() && frontRight.isBusy() && backLeft.isBusy() && backRight.isBusy()) {
-                DisplayDriveTelemetry();
-            }
-            DrivePower(0);
-            Thread.sleep(pause);
-        }
-    }
-
-    public void Strafe(int distance, double power, int pause) throws InterruptedException {
-        if (opModeIsActive()) {
-            StopAndResetDriveEncoders();
-            StrafeByInches(distance);
-            RunDriveToPosition();
-            DrivePower(power);
-            while (frontLeft.isBusy() && frontRight.isBusy() && backLeft.isBusy() && backRight.isBusy()) {
-                DisplayDriveTelemetry();
-            }
-            DrivePower(0);
-            Thread.sleep(pause);
-        }
-    }
-
-    public void Turn(int degrees, double power, int pause) throws InterruptedException {
-        if (opModeIsActive()) {
-            StopAndResetDriveEncoders();
-            TurnByAngle(degrees);
-            RunDriveToPosition();
-            DrivePower(power);
-            while (frontLeft.isBusy() && frontRight.isBusy() && backLeft.isBusy() && backRight.isBusy()) {
-                DisplayDriveTelemetry();
-            }
-            DrivePower(0);
-            Thread.sleep(pause);
-        }
-    }
-
-
     public void StopAndResetArmEncoder() {
         arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
@@ -279,38 +420,6 @@ public class AutoControlsMTZ extends LinearOpMode {
     }
     public void ArmPower(double power) {
         arm.setPower(power);
-    }
-
-    public void RaiseArm(int distance, int pause) throws InterruptedException {
-        /**********
-         * Use time based arm controls since the arm falls when the target distance is reached
-         */
-
-        arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        arm.setPower(0.4);
-        //1000 ms = 12 inches
-        sleep(distance * 1000/12);
-        //sleep(1000);
-        arm.setPower(0.2);
-        Thread.sleep(pause);
-    }
-
-    public void LowerArm(int distance, int pause) throws InterruptedException {
-        arm.setPower(0.1);
-        sleep(distance * 100);
-        arm.setPower(0);
-        Thread.sleep(pause);
-    }
-    public void HooksDown() {
-        leftHook.setPosition(0);
-        rightHook.setPosition(0);
-        sleep(1500);
-    }
-
-    public void HooksUp() {
-        leftHook.setPosition(0.5);
-        rightHook.setPosition(0.5);
-        sleep(1500);
     }
 
 }
